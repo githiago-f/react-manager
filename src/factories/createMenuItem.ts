b056cf4918833f4b4549a704aadb1a1481d66b35
githiago-f <tfarias@protonmail.com>
2020-08-28 00:31:55 -0300

    creating eslint and configuring

--- src/factories/createMenuItem.ts
+++ src/factories/createMenuItem.ts
@@ -2,5 +2,5 @@ import {MenuItem} from 'electron';
 import { MenuItemConstructorOptions } from 'electron/main';
 
 export const createMenuItem = (options: MenuItemConstructorOptions) => {
-  return new MenuItem(options)
-}
+  return new MenuItem(options);
+};
