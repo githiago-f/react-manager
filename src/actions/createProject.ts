c563e3d447fbbc4135b99535fef6ba6dcee526dc
Thiago <thiago.dutra@shareprime.com.br>
2020-08-28 12:06:16 -0300

    adicionando actions

--- src/actions/createProject.ts
+++ src/actions/createProject.ts
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 
 export const openCode = (projectPath: string) => {
-  const codeProccess = exec('code -n .', { cwd: projectPath });
+  const codeProccess = exec('', { cwd: projectPath });
   codeProccess.stderr.on('data', console.log);
 };
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import { app } from 'electron';
 import { createMenu } from './factories/createMenu';
 import { createMenuItem } from './factories/createMenuItem';
 import { createApplication } from './factories/createApplication';
+import { openCode } from './actions/openOnCode';
 
 
 app.on('ready', () => {
   const menu = createMenu([
-    createMenuItem({label: 'React', type: 'normal', click: async () => {
-      console.log('Clicou!');
-    } })
+    createMenuItem({
+      label: 'Open code',
+      type: 'normal',
+      click: () => openCode(
+        'C:\\Users\\Thiago Dutra\\Documents\\Projects\\Thiago\\react\\digital-wallet'
+      )
+    })
   ]);
 
   createApplication({ menu });
